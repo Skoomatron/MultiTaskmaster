7be74d799b1ec12b631e4c6002e6b5bc082aca7f
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-12-07 05:21:46 -0500

    Initial Commit

---
+++ Assets/Scripts/TItleScreen/GlobeRotate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobeRotate : MonoBehaviour {
+    private Sprite sprite;
+    void Start()
+    {
+        
+    }
+}
