19a3bc8050b3fcab170eb041aa56c5d5ae6fd692
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-12-08 10:08:17 -0500

    Title Screen Globe Rotating

--- Assets/Scripts/TItleScreen/GlobeRotate.cs
+++ Assets/Scripts/TItleScreen/GlobeRotate.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GlobeRotate : MonoBehaviour {
-    [SerializeField] private Sprite _sprite;
-    void Start()
+    [SerializeField] private GameObject go;
+    void Awake()
     {
-        
+    }
+
+    private void Update() {
+        go.transform.RotateAround(transform.position, Vector3.forward, 10 * Time.deltaTime);
     }
 }
