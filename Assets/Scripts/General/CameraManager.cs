2654fa5c5118a2b19a2e363ff898cec27250813a
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2023-01-03 11:15:16 -0500

    Map Camera Following Selected Character

---
+++ Assets/Scripts/General/CameraManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+    public GameObject heroToFollow;
+    
+
+    private void Update() {
+        Vector3 playerPos = heroToFollow.transform.position;
+        transform.position = new Vector3(playerPos.x, playerPos.y, -10);
+    }
+}
